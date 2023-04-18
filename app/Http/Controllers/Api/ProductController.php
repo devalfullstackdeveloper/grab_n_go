44197da5297e49afebcd75de433c5ac213273a3b
ruksarphpdev <127926773+ruksarphpdev@users.noreply.github.com>
2023-04-18 12:14:52 +0530

    added random product functionality in product details api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Product;
+use App\Models\Cart;
+use App\Models\CartProduct;
 use App\Models\ProductsImage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use Auth;
 
 class ProductController extends Controller
 {
@@ -42,12 +45,72 @@ class ProductController extends Controller
                 'quantity' =>$product->quantity,
                 'sale' =>$product->sale,
                 'sale_price' =>isset($product->sale_price) ? $product->sale_price : null,
+                'packet' =>isset($product->packet) ? $product->packet : null,
                 'product_image' =>$productImageData,
                 
             );
             }
             
+            //Show Buy it Again Products Data
+            $userId = Auth::user()->id;
 
+            $cartData = Cart::select('cart_product.product_id')->distinct()
+                ->join('cart_product','cart_product.cart_id','=','cart.id')
+                ->where('status',2)
+                ->where('user_id',$userId)
+                ->limit(10)
+                ->get()
+                ->toArray();
+
+            $product = array();
+            foreach ($cartData as $key => $value) {
+                
+
+                    $productDataValue = Product::select()->where('id',$value['product_id'])->get()->toArray();
+
+                    $productImage = ProductsImage::select()->where('product_id',$productDataValue[0]['id'])->get()->toArray();
+
+
+
+                        $product[] = array(
+                            'product_id' =>$productDataValue[0]['id'],
+                            'product_name' =>$productDataValue[0]['product_name'],
+                            'product_price' =>$productDataValue[0]['product_price'],
+                            'product_image' =>$baseUrl['base_url'].$productImage[0]['product_image'],
+                            'product_price' =>$productDataValue[0]['product_price'],
+                            'product_quantity' => $productDataValue[0]['quantity'],
+                            'points' => $productDataValue[0]['point'],
+                            'sale' =>$productDataValue[0]['sale'],
+                            'sale_price' =>$productDataValue[0]['sale_price'],
+                            'packet' =>$productDataValue[0]['packet'],
+                            'sale_price' =>isset($productDataValue[0]->sale_price) ? $productDataValue[0]->sale_price : null,
+                        );
+            }
+
+            //Show Random Products Data
+            $randomProducts = Product::inRandomOrder()->where('status',1)->limit(10)->get()->toArray();
+    
+                $randomProductsViews = array();
+                
+                foreach ($randomProducts as $randomProductData) {
+
+                    $randomProductImage = ProductsImage::select()->where('product_id',$randomProductData['id'])->get()->toArray();
+
+                    $randomProductsViews[] = array(
+                        'product_id' =>$randomProductData['id'],
+                        'product_name' =>$randomProductData['product_name'],
+                        'product_price' =>$randomProductData['product_price'],
+                        'product_image' =>$baseUrl['base_url'].$randomProductImage[0]['product_image'],
+                        'product_price' =>$randomProductData['product_price'],
+                        'product_quantity' => $randomProductData['quantity'],
+                        'sale' => $randomProductData['sale'],
+                        'sale_price' => $randomProductData['sale_price'],
+                        'packet' => $randomProductData['packet'],
+                    );      
+                }
+        
+
+            //Show New Products Data
             $productViewData = Product::latest()->where('status',1)->take(10)->get()->toArray();
         
                 $productViews = array();
@@ -74,10 +137,16 @@ class ProductController extends Controller
             );
 
             if(count($productData) > 0){
-                $returnArr['product'] = $productData;
+                $returnArr['product_details'] = $productData;
+            }
+            if($product){
+            $returnArr['buy_it_again'] = $product;
+            }
+            if(count($randomProductsViews) > 0){
+                $returnArr['products_you_like'] = $randomProductsViews;
             }
             if(count($productViews) > 0){
-                $returnArr['new_producto'] = $productViews;
+                $returnArr['new_product'] = $productViews;
             }
 
             return response()->json($returnArr);
